e681d2b9c542eb398ca0eeb45c7e6434c1b25458
mbjadhav <mjadhav@anl.gov>
2023-03-02 17:59:22 -0600

    interlock relay pcb class completed

--- relayboard/RelayBoard.py
+++ relayboard/RelayBoard.py
@@ -5,6 +5,7 @@ import re
 import serial
 import sys
 import time
+import math
 
 class RelayBoard():
     def __init__(self,  channel=0, value=0, stack=0):
@@ -22,7 +23,7 @@ class RelayBoard():
         command = "ioplus {} relwr {} {}".format(self.stack,channel,value)
         os.system(command)
 
-    def relrd(self, channel):
+    def relrd(self, channel): #value 0 - off, 1 - on
         command = "ioplus {} relrd {}".format(self.stack,channel)
         relread= subprocess.check_output(command, shell=True)
         relread = relread.decode('utf-8')
@@ -35,22 +36,22 @@ class RelayBoard():
         command = "ioplus {} gpiowr {} {}".format(self.stack,channel,value)
         os.system(command)
 
-    def gpiord(self, channel):
+    def gpiord(self, channel): #value 0 - off, 1 - on
         command = "ioplus {} gpiord {}".format(self.stack,channel)
         gpioread= subprocess.check_output(command, shell=True)
         gpioread = gpioread.decode('utf-8')
-        #relrd = re.findall(r'\d+\.\d+', gpioread)
+        #gpiord = re.findall(r'\d+\.\d+', gpioread)
         return int(gpioread)
 
     def gpiodirwr(self, channel, value): #direction value 0 - out, 1 - in
         command = "ioplus {} gpiodirwr {} {}".format(self.stack,channel,value)
         os.system(command)
 
-    def gpiodirrd(self, channel):
+    def gpiodirrd(self, channel): #direction value 0 - out, 1 - in
         command = "ioplus {} gpiodirrd {}".format(self.stack,channel)
         gpiodirread= subprocess.check_output(command, shell=True)
         gpiodirread = gpiodirread.decode('utf-8')
-        #relrd = re.findall(r'\d+\.\d+', gpiodirread)
+        #gpiodirrd = re.findall(r'\d+\.\d+', gpiodirread)
         return int(gpiodirread)
 
     #ADC input & DAC output voltage commands
@@ -59,19 +60,53 @@ class RelayBoard():
         command = "ioplus {} dacwr {} {}".format(self.stack,channel,value)
         os.system(command)
 
+    def dacrd(self, channel): #Read DAC output voltage value (0 - 10V)
+        command = "ioplus {} dacrd {}".format(self.stack,channel)
+        dacread= subprocess.check_output(command, shell=True)
+        dacread = dacread.decode('utf-8')
+        #dacrd = re.findall(r'\d+\.\d+', dacread)
+        return float(dacread)
+
     def adcrd(self, channel): #Read ADC input voltage value (0 - 3.3V)
         command = "ioplus {} adcrd {}".format(self.stack,channel)
         adcread= subprocess.check_output(command, shell=True)
-        adcread = gpioread.decode('utf-8')
-        #relrd = re.findall(r'\d+\.\d+', adcread)
+        adcread = adcread.decode('utf-8')
+        #adcrd = re.findall(r'\d+\.\d+', adcread)
         return float(adcread)
 
     #Optocoupled inputs commands
     #-------------------------------------------------------------------------------------
-
-
-    #output voltage commands
-    #-------------------------------------------------------------------------------------
+    def optrd(self, channel): #Read optocoupled input values (0 - 1)
+        command = "ioplus {} optrd {}".format(self.stack,channel)
+        optread= subprocess.check_output(command, shell=True)
+        optread = optread.decode('utf-8')
+        #optrd = re.findall(r'\d+\.\d+', optread)
+        return int(optread)
 
     #Open drain output commands
     #-------------------------------------------------------------------------------------
+    def odwr(self, channel, value): #Write open drain output pwm value (0% - 100%)
+        command = "ioplus {} odwr {} {}".format(self.stack,channel,value)
+        os.system(command)
+
+    def odrd(self, channel): #Read open drain output pwm value (0% - 100%)
+        command = "ioplus {} odrd {}".format(self.stack,channel)
+        odread= subprocess.check_output(command, shell=True)
+        odread = odread.decode('utf-8')
+        #odrd = re.findall(r'\d+\.\d+', odread)
+        return float(odread)
+
+    #Temperature calculation for the chuck (PT102)
+    #-------------------------------------------------------------------------------------
+    def getTemperature(self, channel):
+        Vin = 3.3
+        Rref = 1760
+        BetaValue = 3892
+        R25 = 1000
+        T25 = 298.15
+        Tk2c = 275.15
+        Vout_ntc = self.adcrd(channel)
+        Rntc = Rref*(Vout_ntc/(Vin-Vout_ntc))
+        Tntc = 1/(math.log10(Rntc/R25)/BetaValue+1/T25)- Tk2c
+        return Tntc
+
