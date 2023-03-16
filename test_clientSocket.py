86e46673f969fe913f72fa2c2f2ad030065ff95e
mbjadhav <mjadhav@anl.gov>
2023-03-16 18:06:56 -0500

    internet socket successful

--- test_clientSocket.py
+++ test_clientSocket.py
@@ -1,14 +1,18 @@
-import socket #import the socket module
+import socket #import socket module
 
-hostraspia = socket.gethostname() 
-print(hostraspia)
-s = socket.socket() #create a socket object
-host = '192.168.0.215' #Host i.p
-port = 12399 #Reserve a port for your service
-
-s.connect((host,port))
-print(s.recv(1024))
-s.close
+s = socket.socket() #Create a socket object
+#host = socket.gethostname() #Get the local machine name
+host = '192.168.0.218'
+#print(host)
+port = 12399 # Reserve a port for your service
+s.bind((host,port)) #Bind to the port
+s.listen(1) #Wait for the client connection
+while True:
+    c,addr = s.accept() #Establish a connection with the client
+    print('Got connection from')
+    print(addr)
+    c.send(b"Thank you for connecting")
+    c.close()
     
 '''
 TCP_IP = '192.16.0.215' # this IP of my pc. When I want raspberry pi 2`s as a client, I replace it with its IP '169.254.54.195'
